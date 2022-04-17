5126ef5b6e56d0e857f9a62957cdc9e63d71208e
who-man-tech <enes515254@gmail.com>
2022-04-17 12:00:05 +0200

    6. React Library. Middle level/react-gotapp: Added show random char button to App component.

--- 6. React library. Middle level/react-gotapp/src/components/app/app.js
+++ 6. React library. Middle level/react-gotapp/src/components/app/app.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import {Col, Row, Container} from 'reactstrap';
+import {Col, Row, Container, Button} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
 
 
-const App = () => {
-    return (
-        <>
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showRandomChar: false,
+        };
+    }
+
+    handleRandomCharButton = (event) => {
+        this.setState({
+            showRandomChar: !this.state.showRandomChar
+        });
+    }
+
+    render() {
+        return (
+            <>
             <Container >
                 <Header />
             </Container>
             <Container>
                 <Row>
                     <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
+                        {this.state.showRandomChar ? <RandomChar class="random-char"/> : null}
+                        <Button color='primary' style={{width: 200, height: 40, margin: 10}} onClick={this.handleRandomCharButton}></Button>
                     </Col>
                 </Row>
                 <Row>
@@ -28,7 +44,9 @@ const App = () => {
                 </Row>
             </Container>
         </>
-    );
-};
+        )
+    }
+
+}
 
 export default App;
