1272e94f48fb9f079e8fe81ca71d3e33cbffca83
who-man-tech <enes515254@gmail.com>
2022-05-08 09:48:58 +0200

    Update structure of charDetails and CharList component to reuse it future pages.
    6. React Library. Middle level/react-gotapp:

---
+++ 6. React library. Middle level/react-gotapp/src/components/itemDetails/itemDetails.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {ListGroup, ListGroupItem} from "react-bootstrap";
+
+
+const Field = ({item, field, label}) => {
+    return (
+        <ListGroupItem className="d-flex justify-content-between">
+            <span className="term">{label}</span>
+            <span>{item[field] || 'Unknown'}</span>
+        </ListGroupItem>
+    );
+}
+
+export {
+    Field
+}
+
+
+export default class ItemDetails extends Component {
+    render() {
+        const {item} = this.props;
+
+        return (
+            <div className="rounded" style={{padding: "25px 25px 15px 25px", marginBottom: 40, backgroundColor: '#fff'}}>
+                {item ? <>
+                    <h4 style={{marginBottom: 20, textAlign: "center"}}>{item.name || 'Unknown'}</h4>
+                    <ListGroup>
+                        {React.Children.map(this.props.children, (child) => React.cloneElement(child, {item}))}
+                    </ListGroup>
+                </> : <span>Please select a character</span>}
+            </div>
+        );
+    }
+}
--- 6. React library. Middle level/react-gotapp/src/components/itemList/itemList.js
+++ 6. React library. Middle level/react-gotapp/src/components/itemList/itemList.js
@@ -1,29 +1,22 @@
 import React, {Component} from 'react';
 import {ListGroup, Spinner} from "react-bootstrap";
-import GotService from "../../services/gotService";
 
 
 export default class ItemList extends Component {
-     gotService = new GotService()
-
-    state = {
-         charList: null,
-    }
-
-    componentDidMount() {
-         this.gotService.getAllCharacters().then(charList => {this.setState({charList})})
-    }
-
     render() {
-        const {charList} = this.state;
+        const {itemList} = this.props;
 
-        if (!charList) {
+        if (!itemList) {
             return <Spinner animation="border" variant="primary" style={{width: 100, height: 100}} />
         }
 
         return (
             <ListGroup>
-                {charList.map((item, i) => item.name ? <ListGroup.Item key={i} onClick={() => this.props.onCharSelected(item.name)}>{item.name}</ListGroup.Item> : null)}
+                {itemList.map((item, i) => {
+                    return <ListGroup.Item key={i} onClick={() => this.props.onItemSelected(item)}>
+                        {this.props.renderItem(item)}
+                    </ListGroup.Item>
+                })}
             </ListGroup>
         );
     }
