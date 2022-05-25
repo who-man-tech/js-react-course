2a0aaab25566eb507f9c40d21a9242425e46de50
who-man-tech <enes515254@gmail.com>
2022-05-25 09:37:40 +0200

    Added menuRequested action and connected MenuList, MenuListItem to api
    RESTO APP

--- resto-app/src/components/app/app.js
+++ resto-app/src/components/app/app.js
@@ -3,10 +3,9 @@ import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
 
 import Background from './food-bg.jpg';
-import WithRestoService from "../hoc";
 import {Route, Routes} from "react-router-dom";
 
-const App = ({restoService}) => {
+const App = () => {
     return (
         <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
             <AppHeader total={50}/>
@@ -18,4 +17,4 @@ const App = ({restoService}) => {
     )
 }
 
-export default WithRestoService()(App);
+export default App;
